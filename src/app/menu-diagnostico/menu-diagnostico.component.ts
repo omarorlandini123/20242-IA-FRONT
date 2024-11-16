4ec95eb9f46941fd67bda3cfee6e309dde2a9513
Jhony <jhonymunozmontoya@gmail.com>
2024-11-16 02:13:55 -0500

    menu de evaluaciones

--- src/app/menu-diagnostico/menu-diagnostico.component.ts
+++ src/app/menu-diagnostico/menu-diagnostico.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AppComponent } from '../app.component';
+import { InicioComponent } from '../inicio/inicio.component';
 
 @Component({
   selector: 'app-menu-diagnostico',
   standalone: true,
-  imports: [RouterLink, AppComponent],
+  imports: [RouterLink, AppComponent, InicioComponent],
   templateUrl: './menu-diagnostico.component.html',
   styleUrl: './menu-diagnostico.component.scss'
 })
